b49cd13a73867ef2815c8935b526ebb8f74e6373
uditham <Uditha.Maduranga@syscolabs.com>
2019-12-03 16:39:19 +0530

    Initial commit

---
+++ oada-formats-viewer/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Oada from "./Views/Format_viewer";
+
+class App extends Component {
+  render() {
+    return <Oada />;
+  }
+}
+
+export default App;
---
+++ oada-formats-viewer/src/Views/Format_viewer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Format_viewer extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hi this is OADA !</p>
+      </div>
+    );
+  }
+}
+
+export default Format_viewer;
