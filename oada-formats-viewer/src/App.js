707936e7be7ef19a5e83392b2241d95000a74ffa
uditham <Uditha.Maduranga@syscolabs.com>
2019-12-04 07:57:02 +0530

    Added dropdown with mock data

--- oada-formats-viewer/src/App.js
+++ oada-formats-viewer/src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Oada from "./Views/Format_viewer";
+import Dropdown from "./Views/Dropdown";
 
 class App extends Component {
   render() {
-    return <Oada />;
+    return (
+      <div>
+        <Oada />
+        <Dropdown />
+      </div>
+    );
   }
 }
 
 export default App;
+if (module.hot) {
+  module.hot.accept();
+}
